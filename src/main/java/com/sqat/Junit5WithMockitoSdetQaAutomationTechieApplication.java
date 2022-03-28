24efba2f99c710bcadd1b703664e387699dcd31c
Sagar Mahapatra <sagarkmohapatra2012@gmail.com>
2022-03-28 20:53:25 +0530

    initial commit

---
+++ src/main/java/com/sqat/Junit5WithMockitoSdetQaAutomationTechieApplication.java
@@ -0,0 +1,13 @@
+package com.sqat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5WithMockitoSdetQaAutomationTechieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5WithMockitoSdetQaAutomationTechieApplication.class, args);
+	}
+
+}
